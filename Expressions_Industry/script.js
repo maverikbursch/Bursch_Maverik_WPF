71c014c26b77c30cc4898b7eb0e6bbfa6e57c541
maverikbursch <maverikbursch@fullsail.edu>
2013-12-06 04:56:26 -0800

    Commit Update//06 December, 2013

--- Expressions_Industry/script.js
+++ Expressions_Industry/script.js
@@ -1,12 +1,14 @@
+//Bursch, Maverik Date: 05 December, 2013 
+
 var bullet1 = prompt("This will calcualte the average bullet overall length of your test lot. \nPlease enter the first bullet OAL:");
 var bullet2 = prompt("Please enter the second bullet OAL:");
 var bullet3 = prompt("Please enter the third bullet OAL:");
 var bullet4 = prompt("Please enter the fourth bullet OAL:");
-var bullet5 = prompt("Please enter the fifth bullet OAL:");
+var bullet5 = prompt("Please enter the fifth bullet OAL:"); 
 
 var average = (Number(bullet1) + Number(bullet2) + Number(bullet3) + Number(bullet4) + Number(bullet5))/5;
 
-alert("Your average OAL is: " + average + " inches");
+alert("Your average OAL for this lot is: " + average + " inches");
 
 console.log(bullet1);
 console.log(bullet2);
