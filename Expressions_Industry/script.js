45a21dfcd9ce3e68900be30362578ea23c9ea7a0
maverikbursch <maverikbursch@fullsail.edu>
2014-02-13 14:12:36 -0800

    Commit #5: Rework of JS file for debug.

--- Expressions_Industry/script.js
+++ Expressions_Industry/script.js
@@ -6,13 +6,3 @@ var bullet3 = prompt("Please enter the third bullet OAL:");
 var bullet4 = prompt("Please enter the fourth bullet OAL:");
 var bullet5 = prompt("Please enter the fifth bullet OAL:"); 
 
-var average = (Number(bullet1) + Number(bullet2) + Number(bullet3) + Number(bullet4) + Number(bullet5))/5;
-
-alert("Your average OAL for this lot is: " + average + " inches");
-
-console.log(bullet1);
-console.log(bullet2);
-console.log(bullet3);
-console.log(bullet4);
-console.log(bullet5);
-console.log(average);
